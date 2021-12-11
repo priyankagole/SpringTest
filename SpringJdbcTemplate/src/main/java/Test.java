d0790e64ca62e736d2957b717e34c8b43043c6c4
Priyanka Gole <priyankagole3@gmail.com>
2021-12-11 13:56:49 +0530

    Update with new configuration

--- SpringJdbcTemplate/src/main/java/Test.java
+++ SpringJdbcTemplate/src/main/java/Test.java
@@ -15,14 +15,21 @@ public class Test {
 		ApplicationContext context= new ClassPathXmlApplicationContext("application-context.xml");
 		
 		EmployeeDao dao= (EmployeeDao)context.getBean("empdao");
-		Employee e= new Employee();
-		e.setId(201);
-		e.setName("Natasha");
-		e.setAge(20);
-		e.setSalary(5000);;
+		
+		  Employee e= new Employee(); 
+		  e.setId(2003); 
+		  e.setName("Neha"); 
+		  e.setAge(20);
+		  e.setSalary(50000);;
+		 
 				
-		int status= dao.saveEmployee(e);
-		System.out.println(status);
+		//Boolean status= dao.saveEmployeeByPreparedStatement(e);
+		//Boolean status= dao.updateEmployeeByPreparedStatement(e);
+		//Boolean status= dao.deleteEmployeeByPreparedStatement(e);
+		//dao.getEmployeesResultSetExtractor().stream().forEach(e->System.out.println(e));
+		//dao.getAllEmployeesByRowMapper().stream().forEach(e->System.out.println(e));
+		dao.saveEmpUsingNamedParam(e);
+		System.out.println();
 	}
 
 }
